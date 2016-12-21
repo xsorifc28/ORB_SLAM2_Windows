605cb561f3bba62b13ab094c83652f99b021833d
Samed Ozdemir <samed@xsor.io>
2016-12-21 18:36:35 -0500

    Update opencv version, fix mono_tum (remove chrono), ignore build files

--- Examples/Monocular/mono_tum.cc
+++ Examples/Monocular/mono_tum.cc
@@ -74,16 +74,16 @@ int main(int argc, char **argv)
                  << string(argv[3]) << "/" << vstrImageFilenames[ni] << endl;
             return 1;
         }
-
+/*
 #ifdef COMPILEDWITHC11
         std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
 #else
         std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
 #endif
-
+*/
         // Pass the image to the SLAM system
         SLAM.TrackMonocular(im,tframe);
-
+/*
 #ifdef COMPILEDWITHC11
         std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
 #else
@@ -100,9 +100,10 @@ int main(int argc, char **argv)
             T = vTimestamps[ni+1]-tframe;
         else if(ni>0)
             T = tframe-vTimestamps[ni-1];
-
+?
         if(ttrack<T)
             usleep((T-ttrack)*1e6);
+*/
     }
 
     // Stop all threads
